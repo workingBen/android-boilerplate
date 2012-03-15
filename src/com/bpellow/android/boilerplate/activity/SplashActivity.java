f0f5f7ff93a24b0f09e5b6a8ab9c446e0c1d4cfb
Ben Pellow <benjamin.pellow@gmail.com>
2012-03-15 11:20:42 -0700

    fix SplashActivity class and R.java

--- src/com/bpellow/android/boilerplate/activity/SplashActivity.java
+++ src/com/bpellow/android/boilerplate/activity/SplashActivity.java
@@ -1,7 +1,9 @@
-package com.bpellow.android.boilerplate;
+package com.bpellow.android.boilerplate.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.bpellow.android.boilerplate.R;
 
 public class SplashActivity extends Activity {
     /** Called when the activity is first created. */
